6950dabe3e768be2de8e2a3aae230c6ad0b8fd15
Matthew Barnard <matt.barnard3@gmail.com>
2019-08-31 14:18:47 -0500

    Building a tankbot!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EV3DevTest1
 Our first test of EV3
+
+
+Matthew and Dequan - 8/30/19
---
+++ TankBot.py
@@ -0,0 +1,10 @@
+#line following robot
+import ev3dev2.motor
+
+
+class Move:
+    tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
+
+
+
+
