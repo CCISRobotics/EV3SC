399f26165bbca01ea703a488f30b708edb8a3406
Matthew Barnard <matt.barnard3@gmail.com>
2019-09-10 18:09:05 -0500

    Main layout for Robotics
    Use this as a template

---
+++ Main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Hello to the world from ev3dev.org'''
+
+import os
+import sys
+import time
+
+
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sound import Sound
+
+#The basics lie here
+#create other python files, etc, this is main
