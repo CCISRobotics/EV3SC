58d226efdd18fbe3d57cfa33eb83ba5f42e6c714
Matthew Barnard <matt.barnard3@gmail.com>
2019-09-19 20:24:10 -0500

    working line follower!

--- Main.py
+++ Main.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
-'''Hello to the world from ev3dev.org'''
+# An EV3 Python (library v2) solution to Exercise 3
+# of the official Lego Robot Educator lessons that
+# are part of the EV3 education software
 
-import os
-import sys
-import time
+from ev3dev2.motor import MoveTank, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.button import Button
+from time import sleep
+5
+btn = Button() # we will use any button to stop script
+tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
 
+# Connect an EV3 color sensor to any sensor port.
+cl = ColorSensor()
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sound import Sound
+while not btn.any():    # exit loop when any button pressed
+    # if we are over the black line (weak reflection)
+    if cl.reflected_light_intensity<30:
+        # medium turn right
+        tank_pair.on(left_speed=50, right_speed=-10)
 
-#The basics lie here
-#create other python files, etc, this is main
+    else:   # strong reflection (>=30) so over white surface
+        # medium turn left
+        tank_pair.on(left_speed=-10, right_speed=50)
+
+    sleep(0.1) # wait for 0.1 seconds
